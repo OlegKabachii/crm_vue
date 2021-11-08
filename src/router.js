0fa38cbdb6fa41574ecc19500029e160f6355ba6
Oleg <kabachiyoleg@gmail.com>
2021-11-08 12:24:29 +0100

    fit: add layouts, views, routs

---
+++ src/router.js
@@ -0,0 +1,72 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+
+
+Vue.use(VueRouter)
+
+const routes = [
+    {
+        path: '/',
+        name: 'home',
+        meta: {layout: 'main'},
+        component: () => import('./views/Home')
+    },
+    {
+        path: '/login',
+        name: 'login',
+        meta: {layout: 'auth'},
+        component: () => import('./views/Login')
+    },
+    {
+        path: '/register',
+        name: 'register',
+        meta: {layout: 'auth'},
+        component: () => import('./views/Register')
+    },
+    {
+        path: '/categories',
+        name: 'categories',
+        meta: {layout: 'main'},
+        component: () => import('./views/Categories')
+    },
+    {
+        path: '/detail/:id',
+        name: 'detail',
+        meta: {layout: 'main'},
+        component: () => import('./views/Detail')
+    },
+    {
+        path: '/history',
+        name: 'history',
+        meta: {layout: 'main'},
+        component: () => import('./views/History')
+    },
+    {
+        path: '/planning',
+        name: 'planning',
+        meta: {layout: 'main'},
+        component: () => import('./views/Planning')
+    },
+    {
+        path: '/profile',
+        name: 'profile',
+        meta: {layout: 'main'},
+        component: () => import('./views/Profile')
+    },
+    {
+        path: '/record',
+        name: 'record',
+        meta: {layout: 'main'},
+        component: () => import('./views/Record')
+    }
+
+]
+
+const router = new VueRouter({
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes
+})
+
+export default router
---
+++ src/views/Detail.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <div>
+      <div class="breadcrumb-wrap">
+        <a href="" class="breadcrumb">History</a>
+        <a class="breadcrumb">
+          Rate
+        </a>
+      </div>
+      <div class="row">
+        <div class="col s12 m6">
+          <div class="card red">
+            <div class="card-content white-text">
+              <p>Description:</p>
+              <p>Sum:</p>
+              <p>Category:</p>
+
+              <small>12.12.12</small>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
